86ca52dfd46b66d5886170b67faae99079454fce
grafiaviles <grafiaviles@gmail.com>
2016-12-13 02:27:56 -0300

    inicio guia js

---
+++ app.js
@@ -0,0 +1,36 @@
+// TIPOS DE DATOS EN VARIABLES
+
+// Numericos
+var eteros = 20; // Enteros
+var decimales = 20.345; // Decimales "Float"
+
+// String 
+var  nombre = 'César'; // Cadena de textos
+var  apellido = 'Avilés'; // Cadena de textos
+
+document.write();
+
+// Arreglos 
+var amigos = ['Cesar', 'Nazo', 'Huevo', 'Pepetapia', 'Yerko', 19, true]; // Almacenan diferentes tipo de datos en una sola variable
+console.log(amigos[5] < 1);
+
+
+// Objetos
+var colores = {
+    color: 'red',
+    backgroundColor: 'black'
+}
+
+// Boleanos - Bolean
+var boleano = false; //Condicionales - Comparaciones
+
+
+// Undefine
+var pandy;
+
+
+// Null * Establecer un valor
+var numero2 = 10; // restablecer
+var numero2 = null; // eliminado
+
+// Nan * Nos muestra cuando se aplica algo ilogico
---
+++ arreglos.js
@@ -0,0 +1,7 @@
+// Es un conjunto de valores que se almacenan
+
+var amigos = ['nazo', 'yerko', 'pepe', 'franko'];
+amigos[4] = 'victor'; // se agrego un valor "victor"
+amigos[2] = 'marco'; // se remplazo el valor "pepe" x "marco"
+
+alert(amigos[0]); //recorrer array
