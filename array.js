19a41a12d638663d30658ca0cf40db7a9ffdc3f5
grafiaviles <grafiaviles@gmail.com>
2016-12-22 01:07:45 -0300

    array

--- array.js
+++ array.js
@@ -1,7 +1,8 @@
-// Es un conjunto de valores que se almacenan
+
+// Almacenan multiples valores
 
 var amigos = ['nazo', 'yerko', 'pepe', 'franko'];
 amigos[4] = 'victor'; // se agrego un valor "victor"
 amigos[2] = 'marco'; // se remplazo el valor "pepe" x "marco"
 
-alert(amigos[0]); //recorrer array
+document.write(amigos[2]);
